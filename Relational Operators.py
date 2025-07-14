a9954e91b743d7a618b0b5c661cf76dba70a77db
Travis Paul Pyle <travispyle@gmail.com>
2025-07-14 15:48:16 -0400

    Add files via upload

---
+++ Relational Operators.py
@@ -0,0 +1,12 @@
+# Relational Operators 
+# equal to operator: ==
+# not equal to operator: !=
+
+# 2 * 2 == 2 + 2
+first_expression = True # this statement is true, as 2*2=4 and 2+2=4 also. Therefore 4 == 4.
+
+# 3 + 3 != 3 * 3
+second_expression = True # this statement is true, as 3+3=6 and 3*3=9. Therefore 6 != 9
+
+# 3 * 3 == '9'
+third_expression = False # this statement is false as '9' is a string. 3*3=9, nine is a integer and integers are not equal to a string  
